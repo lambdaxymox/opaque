481b070735122eef114fbad1bc1b0a5f44f15462
LambdaXymox <lambda.xymox@gmail.com>
2025-04-10 20:09:21 -0500

    Refactor tests.

--- crates/opaque_index_map/tests/common/key_value_generators.rs
+++ crates/opaque_index_map/tests/common/key_value_generators.rs
@@ -1,13 +1,23 @@
-use core::hash;
-
+use core::{fmt, hash};
+use std::ops;
 use opaque_vec::OpaqueVec;
 
 pub fn key_value_pairs<'a, 'b, K, V, I, J>(keys: I, values: J) -> OpaqueVec
 where
     K: Clone + Eq + hash::Hash + 'static,
     V: Clone + Eq + 'static,
-    I: Iterator<Item = &'a K>,
-    J: Iterator<Item = &'b V>,
+    I: Iterator<Item = K>,
+    J: Iterator<Item = V>,
 {
-    OpaqueVec::from_iter(keys.cloned().zip(values.cloned()))
+    OpaqueVec::from_iter(keys.zip(values))
+}
+
+pub fn entries<K, V>(keys: ops::RangeInclusive<K>, values: ops::RangeInclusive<V>) -> OpaqueVec
+where
+    K: Clone + Eq + hash::Hash + fmt::Debug  + 'static,
+    V: Clone + Eq + fmt::Debug + 'static,
+    ops::RangeInclusive<K>: DoubleEndedIterator<Item = K>,
+    ops::RangeInclusive<V>: DoubleEndedIterator<Item = V>,
+{
+    key_value_pairs(keys, values)
 }
