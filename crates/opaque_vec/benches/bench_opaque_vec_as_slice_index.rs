9ea9c2f03470176de3b71582bdf53d90b19e2a43
LambdaXymox <lambda.xymox@gmail.com>
2025-04-11 00:54:43 -0500

    Add new benchmark.

---
+++ crates/opaque_vec/benches/bench_opaque_vec_as_slice_index.rs
@@ -0,0 +1,33 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use opaque_vec::OpaqueVec;
+
+fn bench_vec_as_slice_index(c: &mut Criterion) {
+    let dummy_data = 0_i32;
+    let mut vec = vec![dummy_data; 10000];
+
+    c.bench_function("vec_as_slice_index", |b| {
+        b.iter(|| {
+            let slice = vec.as_slice();
+            for i in 0..slice.len() {
+                let _ = criterion::black_box(slice[i]);
+            }
+        });
+    });
+}
+
+fn bench_opaque_vec_as_slice_index(c: &mut Criterion) {
+    let dummy_data = 0_i32;
+    let opaque_vec = OpaqueVec::from_iter((0..10000).map(|_| dummy_data));
+
+    c.bench_function("opaque_vec_as_slice_index", |b| {
+        b.iter(|| {
+            let slice = opaque_vec.as_slice::<i32>();
+            for i in 0..slice.len() {
+                let _ = criterion::black_box(slice[i]);
+            }
+        });
+    });
+}
+
+criterion_group!(opaque_vec_benches, bench_opaque_vec_as_slice_index, bench_vec_as_slice_index);
+criterion_main!(opaque_vec_benches);
