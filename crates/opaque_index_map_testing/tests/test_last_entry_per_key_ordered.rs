d0edbfe361ec350ff1015a7b13481e6655b443c8
LambdaXymox <lambda.xymox@gmail.com>
2025-04-29 14:04:02 -0500

    Minor refactoring.

--- crates/opaque_index_map_testing/tests/test_last_entry_per_key_ordered.rs
+++ crates/opaque_index_map_testing/tests/test_last_entry_per_key_ordered.rs
@@ -4,7 +4,7 @@ use opaque_index_map_testing as oimt;
 fn test_last_entry_per_key1() {
     let entries: Vec<(i32, i32)> = vec![];
     let expected = vec![];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
@@ -17,7 +17,7 @@ fn test_last_entry_per_key2() {
     let expected = vec![
         (1, 2)
     ];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
@@ -30,7 +30,7 @@ fn test_last_entry_per_key3() {
     let expected = vec![
         (1, 4)
     ];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
@@ -45,7 +45,7 @@ fn test_last_entry_per_key4() {
         (1, 4),
         (2, 5),
     ];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
@@ -72,7 +72,7 @@ fn test_last_entry_per_key5() {
         (7, 17),
         (8, 18),
     ];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
@@ -113,7 +113,7 @@ fn test_last_entry_per_key6() {
         (14, 34),
         (15, 38),
     ];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
@@ -142,22 +142,63 @@ fn test_last_entry_per_key7() {
     ];
     let expected = vec![
         (1, 4),
+        (9, 26),
         (2, 5),
         (3, 7),
         (4, 12),
         (5, 15),
         (6, 16),
+        (10, 30),
         (7, 17),
         (8, 18),
-        (9, 26),
-        (10, 30),
         (11, 31),
+        (15, 38),
         (12, 32),
         (13, 33),
         (14, 34),
-        (15, 38),
     ];
-    let result = oimt::last_entry_per_key(&entries);
+    let result = oimt::last_entry_per_key_ordered(&entries);
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_last_entry_per_key8() {
+    let entries = vec![
+        (15, 38),
+        (14, 34),
+        (13, 33),
+        (12, 32),
+        (11, 31),
+        (10, 30),
+        (9, 26),
+        (8, 18),
+        (7, 17),
+        (6, 16),
+        (5, 15),
+        (4, 12),
+        (3, 7),
+        (2, 5),
+        (1, 4),
+    ];
+    let expected = vec![
+        (15, 38),
+        (14, 34),
+        (13, 33),
+        (12, 32),
+        (11, 31),
+        (10, 30),
+        (9, 26),
+        (8, 18),
+        (7, 17),
+        (6, 16),
+        (5, 15),
+        (4, 12),
+        (3, 7),
+        (2, 5),
+        (1, 4),
+    ];
+    let result = oimt::last_entry_per_key_ordered(&entries);
 
     assert_eq!(result, expected);
 }
