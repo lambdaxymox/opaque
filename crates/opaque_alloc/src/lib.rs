939d61c74c8d98ac89e051f9c3292f8b8acd82a5
LambdaXymox <lambda.xymox@gmail.com>
2025-05-16 13:56:23 -0500

    Minor refactoring.

--- crates/opaque_alloc/src/lib.rs
+++ crates/opaque_alloc/src/lib.rs
@@ -2,20 +2,88 @@
 #![feature(alloc_layout_extra)]
 #![feature(optimize_attribute)]
 use std::alloc;
-use std::alloc::{
-    Allocator,
-    Layout,
-};
+use std::alloc::Allocator;
 use std::any;
 use std::any::TypeId;
 use std::fmt;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
 
-pub trait AnyAllocator: any::Any + Allocator {}
+#[repr(C)]
+struct TypedProjAllocInner<A> {
+    alloc: Box<A>,
+    alloc_type_id: TypeId,
+}
 
-impl<A> AnyAllocator for A where A: any::Any + Allocator {}
+impl<A> TypedProjAllocInner<A> {
+    #[inline]
+    const fn alloc_type_id(&self) -> TypeId {
+        self.alloc_type_id
+    }
+}
 
+impl<A> TypedProjAllocInner<A>
+where
+    A: any::Any + alloc::Allocator,
+{
+    #[inline]
+    fn new(alloc: A) -> Self {
+        let boxed_alloc = Box::new(alloc);
+        let alloc_type_id: TypeId = TypeId::of::<A>();
+
+        Self { alloc: boxed_alloc, alloc_type_id, }
+    }
+
+    #[inline]
+    fn from_boxed_alloc(alloc: Box<A>) -> Self {
+        let alloc_type_id = TypeId::of::<A>();
+
+        Self { alloc, alloc_type_id, }
+    }
+
+    #[inline]
+    fn allocator(&self) -> &A {
+        self.alloc.as_ref()
+    }
+
+    fn into_boxed_alloc(self) -> Box<A> {
+        self.alloc
+    }
+}
+
+impl<A> Clone for TypedProjAllocInner<A>
+where
+    A: any::Any + alloc::Allocator + Clone,
+{
+    fn clone(&self) -> Self {
+        let cloned_alloc = self.alloc.clone();
+
+        Self::from_boxed_alloc(cloned_alloc)
+    }
+}
+
+unsafe impl<A> alloc::Allocator for TypedProjAllocInner<A>
+where
+    A: any::Any + alloc::Allocator,
+{
+    #[inline]
+    fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, alloc::AllocError> {
+        self.alloc.allocate(layout)
+    }
+
+    #[inline]
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
+        unsafe {
+            self.alloc.deallocate(ptr, layout);
+        }
+    }
+}
+
+trait AnyAllocator: any::Any + alloc::Allocator {}
+
+impl<A> AnyAllocator for A where A: any::Any + alloc::Allocator {}
+
+#[repr(C)]
 struct OpaqueAllocInner {
     alloc: Box<dyn AnyAllocator>,
     alloc_type_id: TypeId,
@@ -29,37 +97,21 @@ impl OpaqueAllocInner {
 }
 
 impl OpaqueAllocInner {
-    #[inline]
-    fn new<A>(alloc: A) -> Self
+    pub(crate) fn as_proj_assuming_type<A>(&self) -> &TypedProjAllocInner<A>
     where
         A: any::Any + Allocator,
     {
-        let boxed_alloc = Box::new(alloc);
-        let alloc_type_id: TypeId = TypeId::of::<A>();
-
-        Self { alloc: boxed_alloc, alloc_type_id, }
+        unsafe { &*(self as *const OpaqueAllocInner as *const TypedProjAllocInner<A>) }
     }
 
-    #[inline]
-    fn from_boxed_alloc<A>(alloc: Box<A>) -> Self
+    pub(crate) fn as_proj_mut_assuming_type<A>(&mut self) -> &mut TypedProjAllocInner<A>
     where
         A: any::Any + Allocator,
     {
-        let alloc_type_id = TypeId::of::<A>();
-
-        Self { alloc, alloc_type_id, }
+        unsafe { &mut *(self as *mut OpaqueAllocInner as *mut TypedProjAllocInner<A>) }
     }
 
-    #[inline]
-    fn allocator_assuming_type<A>(&self) -> &A
-    where
-        A: any::Any + Allocator,
-    {
-        let any_alloc = self.alloc.as_ref() as &dyn any::Any;
-        any_alloc.downcast_ref::<A>().unwrap()
-    }
-
-    fn into_boxed_alloc_assuming_type<A>(self) -> Box<A>
+    pub(crate) fn into_proj_assuming_type<A>(self) -> TypedProjAllocInner<A>
     where
         A: any::Any + Allocator,
     {
@@ -68,31 +120,32 @@ impl OpaqueAllocInner {
             Box::from_raw(unboxed_alloc as *mut A)
         };
 
-        boxed_alloc
+        TypedProjAllocInner {
+            alloc: boxed_alloc,
+            alloc_type_id: self.alloc_type_id,
+        }
     }
 
-    fn clone_assuming_type<A>(&self) -> Self
+    #[inline]
+    pub(crate) fn from_proj<A>(proj_self: TypedProjAllocInner<A>) -> Self
     where
-        A: Allocator + any::Any + Clone,
+        A: any::Any + Allocator,
     {
-        let any_alloc = self.alloc.as_ref() as &dyn any::Any;
-        let alloc_ref = any_alloc
-            .downcast_ref::<A>()
-            .unwrap();
-        let cloned_alloc = alloc_ref.clone();
-
-        Self::new(cloned_alloc)
+        Self {
+            alloc: proj_self.alloc,
+            alloc_type_id: proj_self.alloc_type_id,
+        }
     }
 }
 
 unsafe impl alloc::Allocator for OpaqueAllocInner {
     #[inline]
-    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
+    fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, alloc::AllocError> {
         self.alloc.allocate(layout)
     }
 
     #[inline]
-    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
         unsafe {
             self.alloc.deallocate(ptr, layout);
         }
@@ -101,46 +154,45 @@ unsafe impl alloc::Allocator for OpaqueAllocInner {
 
 #[repr(transparent)]
 pub struct TypedProjAlloc<A> {
-    inner: OpaqueAllocInner,
-    _marker: PhantomData<A>,
+    inner: TypedProjAllocInner<A>,
 }
 
 impl<A> TypedProjAlloc<A>
 where
-    A: any::Any + Allocator,
+    A: any::Any + alloc::Allocator,
 {
     #[inline]
     pub fn new(alloc: A) -> Self {
-        let inner = OpaqueAllocInner::new::<A>(alloc);
+        let inner = TypedProjAllocInner::new(alloc);
 
-        Self { inner, _marker: PhantomData }
+        Self { inner, }
     }
 
     #[inline]
     pub fn from_boxed_alloc(alloc: Box<A>) -> Self {
-        let inner = OpaqueAllocInner::from_boxed_alloc(alloc);
+        let inner = TypedProjAllocInner::from_boxed_alloc(alloc);
 
-        Self { inner, _marker: PhantomData }
+        Self { inner, }
     }
 
     pub fn allocator(&self) -> &A {
-        self.inner.allocator_assuming_type::<A>()
+        self.inner.allocator()
     }
 
     pub fn into_boxed_alloc(self) -> Box<A> {
-        self.inner.into_boxed_alloc_assuming_type::<A>()
+        self.inner.into_boxed_alloc()
     }
 }
 
 unsafe impl<A> alloc::Allocator for TypedProjAlloc<A>
 where
-    A: any::Any + Allocator,
+    A: any::Any + alloc::Allocator,
 {
-    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
+    fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
         self.inner.allocate(layout)
     }
 
-    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
         unsafe {
             self.inner.deallocate(ptr, layout);
         }
@@ -149,30 +201,29 @@ where
 
 impl<A> Clone for TypedProjAlloc<A>
 where
-    A: Allocator + any::Any + Clone,
+    A: any::Any + alloc::Allocator + Clone,
 {
     fn clone(&self) -> Self {
         Self {
-            inner: self.inner.clone_assuming_type::<A>(),
-            _marker: self._marker,
+            inner: self.inner.clone(),
         }
     }
 }
 
 impl<A> fmt::Debug for TypedProjAlloc<A>
 where
-    A: Allocator + any::Any + fmt::Debug,
+    A: any::Any + alloc::Allocator + fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("TypedProjAlloc")
-            .field("inner", self.inner.allocator_assuming_type::<A>())
+            .field("inner", self.inner.allocator())
             .finish()
     }
 }
 
 impl<A> Default for TypedProjAlloc<A>
 where
-    A: Allocator + any::Any + Default,
+    A: any::Any + alloc::Allocator + Default,
 {
     fn default() -> Self {
         Self::new(Default::default())
@@ -181,7 +232,7 @@ where
 
 impl<A> From<A> for TypedProjAlloc<A>
 where
-    A: any::Any + Allocator,
+    A: any::Any + alloc::Allocator,
 {
     fn from(alloc: A) -> Self {
         Self::new(alloc)
@@ -202,7 +253,7 @@ impl OpaqueAlloc {
     #[inline]
     pub fn has_alloc_type<A>(&self) -> bool
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         self.inner.alloc_type_id() == TypeId::of::<A>()
     }
@@ -211,7 +262,7 @@ impl OpaqueAlloc {
     #[track_caller]
     fn assert_type_safety<A>(&self)
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         #[cold]
         #[optimize(size)]
@@ -230,7 +281,7 @@ impl OpaqueAlloc {
     #[inline]
     pub fn new<A>(alloc: A) -> Self
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         let proj_alloc = TypedProjAlloc::<A>::new(alloc);
 
@@ -240,7 +291,7 @@ impl OpaqueAlloc {
     #[inline]
     pub fn from_boxed_alloc<A>(alloc: Box<A>) -> Self
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         let proj_alloc = TypedProjAlloc::<A>::from_boxed_alloc(alloc);
 
@@ -251,7 +302,7 @@ impl OpaqueAlloc {
 impl OpaqueAlloc {
     pub fn as_proj<A>(&self) -> &TypedProjAlloc<A>
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         self.assert_type_safety::<A>();
 
@@ -260,7 +311,7 @@ impl OpaqueAlloc {
 
     pub fn as_proj_mut<A>(&mut self) -> &mut TypedProjAlloc<A>
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         self.assert_type_safety::<A>();
 
@@ -269,33 +320,32 @@ impl OpaqueAlloc {
 
     pub fn into_proj<A>(self) -> TypedProjAlloc<A>
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         self.assert_type_safety::<A>();
 
         TypedProjAlloc {
-            inner: self.inner,
-            _marker: std::marker::PhantomData,
+            inner: self.inner.into_proj_assuming_type::<A>(),
         }
     }
 
     #[inline]
     pub fn from_proj<A>(proj_self: TypedProjAlloc<A>) -> Self
     where
-        A: any::Any + Allocator,
+        A: any::Any + alloc::Allocator,
     {
         Self {
-            inner: proj_self.inner,
+            inner: OpaqueAllocInner::from_proj(proj_self.inner),
         }
     }
 }
 
 unsafe impl alloc::Allocator for OpaqueAlloc {
-    fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
+    fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
         self.inner.allocate(layout)
     }
 
-    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
         unsafe {
             self.inner.deallocate(ptr, layout);
         }
