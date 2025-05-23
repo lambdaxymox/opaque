a03ef0d36eb30848561a8333e71c31433eba968d
LambdaXymox <lambda.xymox@gmail.com>
2025-05-22 23:24:45 -0500

    Minor refactoring.

---
+++ crates/opaque_index_map_testing_support/tests/lib.rs
@@ -0,0 +1,4 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+mod test_constant_key_entries;
+mod test_last_entry_per_key_ordered;
+mod test_range_entries;
---
+++ crates/opaque_vec_testing_support/tests/lib.rs
@@ -0,0 +1,4 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+mod test_alternating_values;
+mod test_constant_values;
+mod test_range_values;
