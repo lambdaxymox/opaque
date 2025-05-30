bc786e0f6cc7408c6354a205cd089d41d79d7bba
LambdaXymox <lambda.xymox@gmail.com>
2025-05-29 22:38:31 -0500

    Remove dead code.

--- crates/opaque_index_map/src/map_inner.rs
+++ crates/opaque_index_map/src/map_inner.rs
@@ -803,25 +803,7 @@ where
         formatter.debug_list().entries(self).finish()
     }
 }
-/*
-fn slice_eq<T, U, F>(this: &[T], that: &[U], eq: F) -> bool
-where
-    F: Fn(&T, &U) -> bool,
-{
-    if this.len() != that.len() {
-        return false;
-    }
 
-    // PERFORMANCE: Bounds checks are optimized away.
-    for i in 0..this.len() {
-        if !eq(&this[i], &that[i]) {
-            return false;
-        }
-    }
-
-    true
-}
-*/
 impl<K, V, K2, V2> PartialEq<Slice<K2, V2>> for Slice<K, V>
 where
     K: PartialEq<K2>,
